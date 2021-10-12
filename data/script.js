cd42d6a36c5117a04c463e7079d86c1961e1fa01
Kvz666 <55993619+Kvz666@users.noreply.github.com>
2021-10-12 21:58:07 +0200

    Added mathOperations and first color theme

--- data/script.js
+++ data/script.js
@@ -1,7 +1,7 @@
-const display1el = document.querySelector('.display-1');
-const display2el = document.querySelector('.display-2');
+const display1El = document.querySelector('.display-1');
+const display2El = document.querySelector('.display-2');
 const tempResult = document.querySelector('.temp-result');
-const buttonEl = document.querySelectorAll('.button');
+const numberEl = document.querySelectorAll('.number');
 const operationEl = document.querySelectorAll('.operation');
 const equalEl = document.querySelector('.equal');
 const clearEl = document.querySelector('.all-clear');
@@ -13,7 +13,7 @@ let result = null;
 let lastOperation = '';
 let haveDot = false;
 
-buttonEl.forEach( number => {
+numberEl.forEach( number => {
     number.addEventListener('click', (e) => {
         if( e.target.innerText === '.' && !haveDot){
             haveDot = true;
@@ -21,6 +21,53 @@ buttonEl.forEach( number => {
             return;
     }
     dis2Num += e.target.innerText;
-    display2el.innerText = dis2Num;
+    display2El.innerText = dis2Num;
 })
 });
+
+operationEl.forEach(operation => {
+    operation.addEventListener('click', (e) => {
+        if (!dis2Num ) result;
+        haveDot = false;
+        const operationName = e.target.innerText;
+        if( dis1Num && dis2Num && lastOperation){
+            mathOperation();
+        }else {
+            result = parseFloat(dis2Num);
+        }
+        clearVar(operationName);
+        lastOperation = operationName;
+        console.log(result);
+    })
+});
+
+function clearVar(name = ''){
+    dis1Num += dis2Num + '' + name + '';
+    display1El.innerText = dis1Num;
+    display2El.innerText = '';
+    dis2Num = '';
+    tempResult.innerText = result;
+}
+
+function mathOperation() {
+    if(lastOperation === 'X') {
+        result = parseFloat(result) * parseFloat(dis2Num);
+    }else if ( lastOperation === '+'){
+        result = parseFloat(result) + parseFloat(dis2Num);
+    }else if ( lastOperation === '-'){
+        result = parseFloat(result) - parseFloat(dis2Num);
+    }else if ( lastOperation === '/'){
+        result = parseFloat(result) / parseFloat(dis2Num);
+    };
+};
+
+equalEl.addEventListener('click', (e) => {
+    if( !dis1Num || !dis2Num ) return;
+    haveDot = false;
+    mathOperation();
+    clearVar();
+    display2El.innerText = result;
+    tempResult.innerText = '';
+    dis2Num = result;
+    dis1Num = '';
+})
