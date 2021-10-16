e71872e85e1dea2f48182a1a9f9ab840d0a3c832
Kvz666 <55993619+Kvz666@users.noreply.github.com>
2021-10-16 04:01:15 +0200

    I refactored my code and used class constructor

--- data/script.js
+++ data/script.js
@@ -1,94 +1,130 @@
-const display1El = document.querySelector('.display-1');
-const display2El = document.querySelector('.display-2');
-const tempResult = document.querySelector('.temp-result');
-const numberEl = document.querySelectorAll('.number');
-const operationEl = document.querySelectorAll('.operation');
-const equalEl = document.querySelector('.equal');
-const clearEl = document.querySelector('.all-clear');
-const clearLastEl = document.querySelector('.last-input-clear');
-
-let dis1Num = '';
-let dis2Num = '';
-let result = null;
-let lastOperation = '';
-let haveDot = false;
-
-numberEl.forEach( number => {
-    number.addEventListener('click', (e) => {
-        if( e.target.innerText === '.' && !haveDot){
-            haveDot = true;
-        }else if(e.target.innerText === '.' && haveDot){
-            return;
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
     }
-    dis2Num += e.target.innerText;
-    display2El.innerText = dis2Num;
-})
-});
 
-operationEl.forEach(operation => {
-    operation.addEventListener('click', (e) => {
-        if (!dis2Num ) result;
-        haveDot = false;
-        const operationName = e.target.innerText;
-        if( dis1Num && dis2Num && lastOperation){
-            mathOperation();
-        }else {
-            result = parseFloat(dis2Num);
+    clear() {
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+    }
+
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    }
+
+    appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return;
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
+
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return;
+        if (this.previousOperand !== '') {
+            this.compute();
+        };
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+    }
+
+    compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operation) {
+            case '+':
+            computation = prev + current
+            break
+            case '-':
+            computation = prev - current
+            break
+            case 'x':
+            computation = prev * current
+            break
+            case '/':
+            computation = prev / current
+            break
+            default:
+                return
         }
-        clearVar(operationName);
-        lastOperation = operationName;
-        console.log(result);
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
+    }
+
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+          integerDisplay = ''
+        } else {
+          integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+        }
+        if (decimalDigits != null) {
+          return `${integerDisplay}.${decimalDigits}`
+        } else {
+          return integerDisplay
+        }
+      }
+
+      updateDisplay() {
+        this.currentOperandTextElement.innerText =
+          this.getDisplayNumber(this.currentOperand)
+        if (this.operation != null) {
+          this.previousOperandTextElement.innerText =
+            `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+          this.previousOperandTextElement.innerText = ''
+        }
+      }
+    }
+
+const numberButtons = document.querySelectorAll('[data-number');
+const operationButtons = document.querySelectorAll('[data-operation');
+const equalButton = document.querySelector('[data-equals');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator (previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+
+    
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
     })
 });
-
-function clearVar(name = ''){
-    dis1Num += dis2Num + '' + name + '';
-    display1El.innerText = dis1Num;
-    display2El.innerText = '';
-    dis2Num = '';
-    tempResult.innerText = result;
-}
-
-function mathOperation() {
-    if(lastOperation === 'X') {
-        result = parseFloat(result) * parseFloat(dis2Num);
-    }else if ( lastOperation === '+'){
-        result = parseFloat(result) + parseFloat(dis2Num);
-    }else if ( lastOperation === '-'){
-        result = parseFloat(result) - parseFloat(dis2Num);
-    }else if ( lastOperation === '/'){
-        result = parseFloat(result) / parseFloat(dis2Num);
-    };
-};
-
-equalEl.addEventListener('click', (e) => {
-    if( !dis1Num || !dis2Num ) return;
-    haveDot = false;
-    mathOperation();
-    clearVar();
-    display2El.innerText = result; 
-    display1El.innerText = '';
-    tempResult.innerText = '';
-    dis2Num = result;
-    dis1Num = '';
+    
+equalButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
 });
 
-
-clearEl.addEventListener('click', (e) => {
-    display1El.innerText = '0';
-    display2El.innerText = '0';
-    dis1Num = '';
-    dis2Num = '';
-    result = '';
-    tempResult.innerText = '0';
+allClearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
 });
 
-clearLastEl.addEventListener('click', (e) =>{
-    display2El.innerText = '';
-    dis2Num = '';
-    tempResult = '';
-})
-
+deleteButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisplay();
+});
 
 function changeTheme(theme)
 {
